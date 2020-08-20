d298baeeeebf889f715ba1c66bec7379f0af2261
chenyuanguang <chenyuanguang@cmcm.com>
2020-08-20 18:42:24 +0800

    refactor(blend-react): 修改port
    affects: blend-react

--- packages/blend-react/bin/start.js
+++ packages/blend-react/bin/start.js
@@ -11,7 +11,7 @@ async function startDevServer() {
     server.use(require('webpack-hot-middleware')(compiler));
 
     try {
-        server.listen(port, async (err) => {
+        server.listen(devServer.port, async (err) => {
             if (err) console.error(err);
             // if (err) buildingError(err);
             // console.log('===========')
