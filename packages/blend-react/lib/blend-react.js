9fc15a44e4d93428061011897ce6133e1db83949
chenyuanguang <chenyuanguang@cmcm.com>
2020-08-20 18:33:22 +0800

    refactor(blend-react): 修改lib文件
    affects: blend-react
    
    null
    
    BREAKING CHANGE:
    null
    
    ISSUES CLOSED:  null

--- packages/blend-react/lib/blend-react.js
+++ packages/blend-react/lib/blend-react.js
@@ -1,11 +1,11 @@
 /*
  * @Author: your name
  * @Date: 2020-08-19 16:10:22
- * @LastEditTime: 2020-08-20 16:55:17
- * @LastEditors: your name
+ * @LastEditTime: 2020-08-20 18:32:45
+ * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /blend/packages/blend-react/lib/blend-react.js
  */
 'use strict';
 
-module.exports = require('./lib');
+module.exports = require('./bin');
